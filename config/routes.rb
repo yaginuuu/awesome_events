675e70a62975986b1365a7749b4679b38c369660
yaginuuu <yaginuuu56@gmail.com>
2014-09-06 05:19:02 +0900

    p223までおｋ(エラーハンドリングはしてない)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
 
+  resource :user do
+    get 'retire'
+  end
+
   resources :events do
     resources :tickets
   end
